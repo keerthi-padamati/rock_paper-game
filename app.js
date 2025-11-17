345e9d1e7403563661e3331a877186fa7644313f
Keerthi <keerthipadamati16@gmail.com>
2025-11-17 18:44:12 +0530

    Update app.js
    added the function get computer choice and also got the objects of rock,paper,scissors etc

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice()
+    return choices[Math.floor(Math.random() * choices.length)];
 
