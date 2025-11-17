85ecf78ea8453b9fd22c22547a7342e9d10e418e
Keerthi <keerthipadamati16@gmail.com>
2025-11-17 18:45:07 +0530

    Update app.js
    added getwinner function

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ const choices = ['rock', 'paper', 'scissors'];
 function getComputerChoice()
     return choices[Math.floor(Math.random() * choices.length)];
 
+function getWinner(player, computer)
+{
+    if(player === computer) 
+        return "It's a tie!";
+    if(
+        (player === 'rock' && computer === 'scissors') ||
+        (player === 'paper' && computer === 'rock') ||
+        (player === 'scissors' && computer === 'paper')
+    )
+        return "You win!";
+    return "Computer wins!";
+}
